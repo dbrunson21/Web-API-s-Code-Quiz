15b252e8010e3c9e4c9a0f0e02e64c09eb21cdcc
dbrunson21 <brunsondavonte@gmail.com>
2023-09-18 19:59:43 -0400

    defined 1st question added subtitle to h1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //define quiz questions and answers
 var quizQuestions = [
     {
-        question: ""
+        question: "Commonly used data types DO NOT include:"
+        choices: ["strings", "booleans", "alerts", "numbers"]
+        correctAnswer: "alerts"
     }
 ]
