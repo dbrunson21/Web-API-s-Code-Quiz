79a0a592046fa7fb6c520b1ee45705538965131d
dbrunson21 <brunsondavonte@gmail.com>
2023-09-19 00:10:02 -0400

    added funtions to show message, choices, ending

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ var quizQuestions = [
 
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScript", "terminal/bash", "for loops", "console.log<"],
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
         correctAnswer: ""
     },
 ];
@@ -77,6 +77,21 @@ function startTimer() {
     }, 1000);
 }
 
+// Function to start the quiz
+function startQuiz() {
+    $("#start-button").hide();
+    currentQuestionIndex = 0;
+    score = 0;
+    showQuestionPage();
+    startTimer();
+}
+
+// Function to show the current question page
+function showQuestionPage() {
+    $(".question").hide();
+    $(`#question${currentQuestionIndex + 1}`).show();
+}
+
 //function to check the answer
 function checkAnswer(selectedChoice) {
     var question = quizQuestions[currentQuestionIndex];
@@ -93,6 +108,42 @@ function checkAnswer(selectedChoice) {
         endQuiz();
     }
 }
+
+// Function to show correct or wrong message
+function showMessage(isCorrect) {
+    const messageElement = $("<p>").attr("id", "message");
+    if (isCorrect) {
+        messageElement.text("Correct!");
+    } else {
+        messageElement.text("Wrong!");
+        timeLeft -= 10; // Penalty for wrong answer
+    }
+    $("#quiz-container").append(messageElement);
+}
+
+// Function to handle click on choice buttons
+$("#quiz-container").on("click", ".choice", function () {
+    const selectedChoice = $(this).text();
+    const question = quizQuestions[currentQuestionIndex];
+
+    if (selectedChoice === question.correctAnswer) {
+        score++;
+        showMessage(true);
+    } else {
+        showMessage(false);
+    }
+
+    setTimeout(function () {
+        $("#message").remove();
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < quizQuestions.length) {
+            showQuestion();
+        } else {
+            endQuiz();
+        }
+    }, 1000); // Delay before moving to the next question
+});
 
 //function to end the quiz
 function endQuiz() {
