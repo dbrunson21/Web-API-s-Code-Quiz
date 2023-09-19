ae6ea4184bdf92db575da6cf7582b76d91c7a723
dbrunson21 <brunsondavonte@gmail.com>
2023-09-18 20:49:30 -0400

    linked js file/ added questions

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ var quizQuestions = [
         question: "Commonly used data types DO NOT include:"
         choices: ["strings", "booleans", "alerts", "numbers"]
         correctAnswer: "alerts"
+    },
+
+    {
+        question: "The condition in an if / else statement is enclosed with _____. "
+        choices: ["quotes", "curly bractets", "parenthesis", "square brackets"]
+        correctAnswer: "parenthesis"
     }
 ]
