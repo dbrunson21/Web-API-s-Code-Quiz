1500ac47cd374651c044e960f6385fea743a8ec0
dbrunson21 <brunsondavonte@gmail.com>
2023-09-18 21:47:21 -0400

    added questrions to js

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ var quizQuestions = [
         question: "The condition in an if / else statement is enclosed with _____. "
         choices: ["quotes", "curly bractets", "parenthesis", "square brackets"]
         correctAnswer: "parenthesis"
+    },
+
+    {
+        question:""
+        choices:["numbers and strings", "other arrays", "booleans", "all of the above"]
+        correctAnswer: "all of the above"
     }
-]
+
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables."
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"]
+        correctAnswer: "quotes"
+    },
+
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:"
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log<"]
+        correctAnswer: ""
+    },
+];
