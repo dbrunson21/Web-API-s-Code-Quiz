3d667094971832d99e5dd2decca30ae2b33597f6
dbrunson21 <brunsondavonte@gmail.com>
2023-09-18 22:57:47 -0400

    functions for timer, check ans. & end quiz

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ var quizQuestions = [
     },
 
     {
-        question:"",
-        choices:["numbers and strings", "other arrays", "booleans", "all of the above"],
+        question: "",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         correctAnswer: "all of the above"
     },
 
@@ -34,11 +34,11 @@ var quizQuestions = [
 //Event listeners
 $("#start-button").click(startQuiz);
 
-$("#quiz-container").on("click", ".choice", function() {
+$("#quiz-container").on("click", ".choice", function () {
     checkAnswer($(this).text());
 });
 
-$("#save-button").click(function() {
+$("#save-button").click(function () {
     var initials = $("#initials").val();
     //send code and initials to the server
     console.log(`Initials: ${initials}, Score: ${score}`);
@@ -58,9 +58,46 @@ function startQuiz() {
 
 //function to display a question
 function showQuestion() {
-    var question = quizQestions[currentQuestionIndex];
+    var question = quizQuestions[currentQuestionIndex];
     $("#quiz-continer h2").text(`Question ${currentQuestionIndex + 1}:`);
-    $("#quiz-container .choice").each(function(index) {
+    $("#quiz-container .choice").each(function (index) {
         $(this).text(question.choices[index]);
     });
+}
+
+//funtion to start the timer
+function startTimer() {
+    timerInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            timeLeft--;
+            $("#time").text(timeLeft);
+        } else {
+            endQuiz();
+        }
+    }, 1000);
+}
+
+//function to check the answer
+function checkAnswer(selectedChoice) {
+    var question = quizQuestions[currentQuestionIndex];
+    if (selectedChoice === question.correctAnswer) {
+        score++;
+    } else {
+        timeLeft -= 10; //penalty for wrong answer
+    }
+
+    currentQuestionIndex++;
+    if (currentQuestionIndex < quizQuestions.length) {
+        showQuestion();
+    } else {
+        endQuiz();
+    }
+}
+
+//function to end the quiz
+function endQuiz() {
+    clearInterval(timerInterval);
+    $("#quiz-container").hide();
+    $("#result-container").show();
+    $("#score").text(score);
 }
